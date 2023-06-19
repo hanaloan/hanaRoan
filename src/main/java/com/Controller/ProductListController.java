958e7ecf9a2610f7e83310cfcdc800c6746c0a15
Changho Sung <84298467+kylesung0520@users.noreply.github.com>
2023-06-19 14:42:48 +0900

    Feat: 상품상세페이지 기능 구현, DB Connector 매소드화 (#13)

--- src/main/java/com/Controller/ProductListController.java
+++ src/main/java/com/Controller/ProductListController.java
@@ -4,14 +4,20 @@ import com.DAO.ProductDao;
 import com.Model.Product;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+@WebServlet("/ProductList")
 public class ProductListController {
+    private ProductDao productDao;
+
+    public void init() {
+        productDao = new ProductDao();
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao = new ProductDao();
         List<Product> products = productDao.getItems();
 
         request.setAttribute("products", products);
