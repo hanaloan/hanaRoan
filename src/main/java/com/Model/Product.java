eec5299c28e3dc1ebd76751358c1c5bdc7dbe5cb
Changho Sung <84298467+kylesung0520@users.noreply.github.com>
2023-06-21 13:50:34 +0900

    feat:  ㅊcreated a categorizing function based on the type of loan products on product list page (#29)
    고생하셨습니다.

--- src/main/java/com/Model/Product.java
+++ src/main/java/com/Model/Product.java
@@ -1,9 +1,14 @@
 package com.Model;
+
+import lombok.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
-    private int id;
+    private String id;
     private String name;
     private String description;
     private int minCredit;
@@ -12,54 +17,4 @@ public class Product {
     private BigDecimal interestRate;
     private Date startDate;
     private Date endDate;
-
-    public Product(int id, String name, String description, int minCredit,
-                   BigDecimal lendLimit, int loanPeriod, BigDecimal interestRate,
-                   Date startDate, Date endDate) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.minCredit = minCredit;
-        this.lendLimit = lendLimit;
-        this.loanPeriod = loanPeriod;
-        this.interestRate = interestRate;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-
-    public int getId() {return id;}
-
-    public void setId(int id) {this.id = id;}
-
-    public String getName() {return name;}
-
-    public void setName(String name) {this.name = name;}
-
-    public String getDescription() {return description;}
-
-    public void setDescription(String description) {this.description = description;}
-
-    public int getMinCredit() {return minCredit;}
-
-    public void setMinCredit(int minCredit) {this.minCredit = minCredit;}
-
-    public BigDecimal getLendLimit() {return lendLimit;}
-
-    public void setLendLimit(BigDecimal lendLimit) {this.lendLimit = lendLimit;}
-
-    public int getLoanPeriod() {return loanPeriod;}
-
-    public void setLendPeriod(int lendPeriod) {this.loanPeriod = lendPeriod;}
-
-    public BigDecimal getInterestRate() {return interestRate;}
-
-    public void setInterestRate(BigDecimal interestRate) {this.interestRate = interestRate;}
-
-    public Date getStartDate() {return startDate;}
-
-    public void setStartDate(Date startDate) {this.startDate = startDate;}
-
-    public Date getEndDate() {return endDate;}
-
-    public void setEndDate(Date endDate) {this.endDate = endDate;}
 }
