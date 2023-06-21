0bb965d4d29572b857b8e08f28cdcfaf58e89ac3
Changho Sung <84298467+kylesung0520@users.noreply.github.com>
2023-06-21 14:29:47 +0900

    Chore: edited ProductDao to release resources that were allocated during database query (#30)

--- src/main/java/com/DAO/ProductDao.java
+++ src/main/java/com/DAO/ProductDao.java
@@ -8,13 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
-    Connection conn = null;
-    PreparedStatement stmt = null;
-    public List<Product> getProducts(String category) {
+    public List<Product> getProducts(String category) throws SQLException {
         List<Product> products = new ArrayList<>();
-        try  {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
             conn = DatabaseConnector.getConnection();
-            String sql = "SELECT * FROM loan_products\n";
+            String sql = "SELECT * FROM loan_products";
 
             if (!category.equals("*")) {
                 sql += " WHERE loan_products.loan_type_id = ?";
@@ -23,54 +25,63 @@ public class ProductDao {
             } else {
                 stmt = conn.prepareStatement(sql);
             }
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Product product = new Product();
-                product.setId(String.valueOf(rs.getInt("loan_idx")));
-                product.setName(rs.getString("loan_name"));
-                product.setDescription(rs.getString("loan_description"));
-                product.setMinCredit(rs.getInt("min_credit"));
-                product.setLendLimit(rs.getBigDecimal("lend_limit"));
-                product.setLoanPeriod(rs.getInt("lend_period"));
-                product.setInterestRate(rs.getBigDecimal("loan_interest_rate"));
-                product.setStartDate(rs.getDate("start_date"));
-                product.setEndDate(rs.getDate("end_date"));
+            rs = stmt.executeQuery();
 
+            while (rs.next()) {
+                Product product = extractProductFromResultSet(rs);
                 products.add(product);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw e;
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
         }
         return products;
     }
 
-    public Product getProductById(int productIdx) {
+    public Product getProductById(int productIdx) throws SQLException {
         Product product = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
         try {
             conn = DatabaseConnector.getConnection();
             String sql = "SELECT * FROM loan_products WHERE loan_idx = ?";
             stmt = conn.prepareStatement(sql);
             stmt.setInt(1, productIdx);
-            ResultSet rs = stmt.executeQuery();
+            rs = stmt.executeQuery();
+
             if (rs.next()) {
-                product = new Product();
-                product.setId(String.valueOf(rs.getInt("loan_idx")));
-                product.setName(rs.getString("loan_name"));
-                product.setDescription(rs.getString("loan_description"));
-                product.setMinCredit(rs.getInt("min_credit"));
-                product.setLendLimit(rs.getBigDecimal("lend_limit"));
-                product.setLoanPeriod(rs.getInt("lend_period"));
-                product.setInterestRate(rs.getBigDecimal("loan_interest_rate"));
-                product.setStartDate(rs.getDate("start_date"));
-                product.setEndDate(rs.getDate("end_date"));
+                product = extractProductFromResultSet(rs);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw e;
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
         }
+        return product;
+    }
+
+    private Product extractProductFromResultSet(ResultSet rs) throws SQLException {
+        Product product = new Product();
+        product.setId(String.valueOf(rs.getInt("loan_idx")));
+        product.setName(rs.getString("loan_name"));
+        product.setDescription(rs.getString("loan_description"));
+        product.setMinCredit(rs.getInt("min_credit"));
+        product.setLendLimit(rs.getBigDecimal("lend_limit"));
+        product.setLoanPeriod(rs.getInt("lend_period"));
+        product.setInterestRate(rs.getBigDecimal("loan_interest_rate"));
+        product.setStartDate(rs.getDate("start_date"));
+        product.setEndDate(rs.getDate("end_date"));
         return product;
     }
 }
