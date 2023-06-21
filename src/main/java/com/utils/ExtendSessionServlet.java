73977c5c4c876cf66cf3f264cae18f51f118dfa1
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-06-21 10:22:43 +0900

    Feat : create sidebar and seesion

---
+++ src/main/java/com/utils/ExtendSessionServlet.java
@@ -0,0 +1,16 @@
+package com.utils;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/extendSession")
+public class ExtendSessionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setMaxInactiveInterval(10*60); //
+    }
+}
