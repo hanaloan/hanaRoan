bd45cfbc2cbf4e2ce1fd13d2a8085db8a0d9fb67
Changho Sung <84298467+kylesung0520@users.noreply.github.com>
2023-06-23 09:41:20 +0900

    Feat: manage loan payment (#35)
    * Feat: created loan repayment managing functions
    
    * Chore: Deleted unused file

--- src/main/java/com/DAO/PaymentDao.java
+++ src/main/java/com/DAO/PaymentDao.java
@@ -5,11 +5,10 @@ import com.utils.DatabaseConnector;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PaymentDao {
-    public List<Payment> getAllPayment() throws SQLException {
+    public List<Payment> getPayments(String option1, String option2) throws SQLException {
         List<Payment> paymentList = new ArrayList<>();
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -17,15 +16,36 @@ public class PaymentDao {
 
         try {
             conn = DatabaseConnector.getConnection();
-            String query = "SELECT lp.repayment_idx, c.name, lpr.loan_type_id, lpr.loan_name, ll.start_date, ll.end_date, lp.payment_amount\n" +
+            String sql = "SELECT lp.repayment_idx, c.name, lpr.loan_type_id, lpr.loan_name, ll.start_date, ll.end_date, lp.payment_amount, lp.payment_status\n" +
                     "FROM loan_payments lp\n" +
                     "JOIN loan_lend ll ON lp.loan_lend_idx = ll.lend_idx\n" +
                     "JOIN loan_products lpr ON ll.loan_idx = lpr.loan_idx\n" +
                     "JOIN customers c ON ll.customer_idx = c.customer_idx\n" +
                     "WHERE ll.loan_status = 'approved'";
-            stmt = conn.prepareStatement(query);
-            rs = stmt.executeQuery();
 
+            String[] option = getPaymentOption(option1, option2);
+            Map<Integer, String> category = new HashMap<>();
+
+            if(!option[0].equals("*") || !option[1].equals("*")){
+                int idx = 1;
+                if(!option[0].equals("*")){
+                    sql += "and lpr.loan_type_id = ?";
+                    category.put(idx++, option[0]);
+                }
+                if(!option[1].equals("*")){
+                    sql += "and lp.payment_status = ?";
+                    category.put(idx++, option[1]);
+                }
+                stmt = conn.prepareStatement(sql);
+                for(int i : category.keySet()){
+                    stmt.setString(i, category.get(i));
+                }
+            }
+            else{
+                stmt = conn.prepareStatement(sql);
+            }
+
+            rs = stmt.executeQuery();
             while (rs.next()) {
                 Payment payment = new Payment();
                 payment.setPaymentId(rs.getInt("repayment_idx"));
@@ -37,6 +57,7 @@ public class PaymentDao {
                 payment.setPaymentDueDate(rs.getDate("end_date"));
                 payment.setDueBalance(rs.getBigDecimal("payment_amount"));
                 payment.setPassedDate(getPassedDate(rs.getString("end_date")));
+                payment.setPaymentStatus(getStatus(rs.getString("payment_status")));
                 paymentList.add(payment);
             }
         } catch (ClassNotFoundException e) {
@@ -46,10 +67,55 @@ public class PaymentDao {
             if (stmt != null) stmt.close();
             if (conn != null) conn.close();
         }
-        System.out.println(paymentList.size());
         return paymentList;
     }
 
+    public void deductBalance(String paymentId) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = DatabaseConnector.getConnection();
+            String sql = "UPDATE loan_payments\n" +
+                    "SET payment_amount = 0\n," +
+                    "    payment_status = 'paid'" +
+                    "WHERE repayment_idx = ?;";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, paymentId);
+            stmt.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        }
+    }
+
+    public void handleOverdue(String paymentId) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = DatabaseConnector.getConnection();
+            String sql = "UPDATE loan_payments as lp\n" +
+                    "JOIN loan_lend as ll ON lp.loan_lend_idx = ll.lend_idx\n" +
+                    "JOIN loan_products as lpr ON ll.loan_idx = lpr.loan_idx\n" +
+                    "SET lp.payment_amount = ll.loan_amount + (ll.loan_amount * lpr.overdue_interest_rate / 100) * CEIL(DATEDIFF(CURDATE(), ll.end_date) / 365),\n" +
+                    "    lp.payment_status = 'overdue'\n" +
+                    "WHERE lp.payment_status != 'paid' AND lp.repayment_idx = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, paymentId);
+            stmt.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        }
+    }
+    /* Helper Functions */
     public String[] getPaymentOption(String option1, String option2){
         String[] retVal = new String[2];
         switch(option1){
@@ -67,13 +133,13 @@ public class PaymentDao {
                 break;
         }
         switch(option2){
-            case "미납중":
+            case "연체":
                 retVal[1] = "overdue";
                 break;
             case "상환중":
                 retVal[1] = "in progress";
                 break;
-            case "상환 완료":
+            case "상환완료":
                 retVal[1] = "paid";
                 break;
             default:
@@ -105,6 +171,20 @@ public class PaymentDao {
 
         long daysDifference = ChronoUnit.DAYS.between(specificDate, today);
 
-        return daysDifference <= 0 ? "-" : String.valueOf(daysDifference);
+        return daysDifference <= 0 ? "0" : String.valueOf(daysDifference);
     }
+
+    public String getStatus(String status){
+        switch(status){
+            case "in progress":
+                return "상환중";
+            case "paid":
+                return "상환완료";
+            case "overdue":
+                return "연체";
+            default:
+                return "-";
+        }
+    }
+
 }
