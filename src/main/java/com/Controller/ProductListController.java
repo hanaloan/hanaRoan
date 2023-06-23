f1420d37edcb385f8466b137d79a3d9c691b302c
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-06-23 16:50:27 +0900

    Fix : (#39)

--- src/main/java/com/Controller/ProductListController.java
+++ src/main/java/com/Controller/ProductListController.java
@@ -1,36 +1,51 @@
 package com.Controller;
 
+import com.Background.RedisScheduler;
 import com.Service.DisplayProductListService;
 import com.Model.Product;
+import com.Service.RedisService;
+import com.Model.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 import java.util.List;
 
 @WebServlet("/productList")
 public class ProductListController extends HttpServlet {
     private DisplayProductListService displayProductListService;
+    private RedisService redisService;
 
     @Override
     public void init() throws ServletException {
         super.init();
         displayProductListService = new DisplayProductListService();
+        redisService = new RedisService("redis://localhost:6379");// Redis URI
+        new RedisScheduler(redisService).start(); // RedisScheduler 시작
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
+            HttpSession session = request.getSession();
+            Integer customer_Idx = (Integer) session.getAttribute("customer_Idx");
+
+
+
             String category = request.getParameter("category");
             if (category == null || category.isEmpty()) {category = "*";}
 
             List<Product> productList = displayProductListService.getProducts(category);
             request.setAttribute("productList", productList);
             request.getRequestDispatcher("jsp/DisplayProduct/ProductList.jsp").forward(request, response);
+
+            redisService.increasePageView(customer_Idx);
         } catch (SQLException e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.getWriter().println("Internal Server Error");
