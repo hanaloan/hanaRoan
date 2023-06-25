3ef34ef1d017eafadaf0326496b4d198f1d066ea
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-06-25 13:41:40 +0900

    Feat : Create logic to run individual PK and total PK in the background

--- src/main/java/com/Background/RedisScheduler.java
+++ src/main/java/com/Background/RedisScheduler.java
@@ -1,36 +1,64 @@
-package com.Background;
-
-import com.Service.RedisService;
-import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class RedisScheduler {
-    private RedisService redisService;
-    private Timer timer;
-
-    public RedisScheduler(RedisService redisService) {
-        this.redisService = redisService;
-        this.timer = new Timer();
-    }
-
-    public void start() {
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Set<String> allUserIds = redisService.getAllKeys();
-                int totalPageViews = 0;
-                // 모든 key값 돌면서 total pv구하기
-                for (String userId : allUserIds) {
-                    try {
-                        int pageViews = redisService.getPageView(userId);
-                        totalPageViews += pageViews;
-                    } catch (NumberFormatException e) {
-                    }
-                }
-                System.out.println("Total page views: " + totalPageViews);
-                // DB 로직 짜기
-            }
-        }, 0, 3000); // 3초마다 실행
-    }
-}
+//package com.Background;
+//
+//import com.Service.RedisService;
+//
+//import javax.servlet.ServletContextEvent;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//public class RedisScheduler {
+//    private RedisService redisService;
+//    private Timer timer;
+//
+//    public RedisScheduler(RedisService redisService) {
+//        this.redisService = redisService;
+//        this.timer = new Timer();
+//    }
+//
+//    public void start() {
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//                //최신 pv 업댓된 pk 구하는 로직
+//                List<String> PreList = redisService.getPreList();
+//                for (String customer_Idx : PreList) {
+//                    try {
+//                        Integer currentPageViews = redisService.getPageView(customer_Idx);
+//                        System.out.println("바뀐 pk는 "+customer_Idx+"이고 pv는 "  + currentPageViews);
+//
+//                    } catch (NumberFormatException e) {
+//                        System.out.println(e);
+//                    }
+//                }
+//
+//                redisService.clearPreList();
+//
+//                // 토탈 pk 구하는 로직
+//                Set<String> allUserIds = redisService.getAllKeys();
+//                int totalPageViews = 0;
+//                // 모든 key값 돌면서 total pv구하기
+//                for (String userId : allUserIds) {
+//                    try {
+//                        Integer pageViews = redisService.getPageView(userId);
+//                        totalPageViews += pageViews;
+//
+//                    } catch (NumberFormatException e) {
+//                        System.out.println(e);
+//                    }
+//                }
+//                System.out.println("Total page views: " + totalPageViews);
+//                // DB 로직 짜기
+//
+//
+//            }
+//        }, 0, 3000); // 3초마다 실행
+//    }
+//
+//    public void close() {
+//        timer.cancel();
+//        timer.purge();
+//    }
+//}
--- src/main/java/com/Service/RedisService.java
+++ src/main/java/com/Service/RedisService.java
@@ -1,49 +1,64 @@
-package com.Service;
-
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.exceptions.JedisException;
-import java.util.Set;
-
-public class RedisService {
-    private JedisPool jedisPool;
-
-    public RedisService(String host) {
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(100); // 최대 커넥션 수
-        poolConfig.setMaxIdle(10); // 유휴 상태에서 유지하는 최대 커넥션 수
-        poolConfig.setMinIdle(5); // 유휴 상태에서 유지하는 최소 커넥션 수
-        poolConfig.setMaxWaitMillis(3000); // 커넥션 풀에서 사용 가능한 커넥션을 기다리는 최대 시간 (밀리초)
-
-        this.jedisPool = new JedisPool(poolConfig, host);
-    }
-
-    public void increasePageView(Integer customer_Idx) {
-        try (Jedis jedis = jedisPool.getResource()) {
-            jedis.incr(String.valueOf(customer_Idx));
-        } catch (JedisException e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
-    public int getPageView(String customer_Idx) {
-        try (Jedis jedis = jedisPool.getResource()) {
-            String value = jedis.get(customer_Idx);
-            return value == null ? 0 : Integer.parseInt(value);
-        } catch (JedisException e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
-    public Set<String> getAllKeys() {
-        try (Jedis jedis = jedisPool.getResource()) {
-            return jedis.keys("*");
-        } catch (JedisException e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
-}
+//package com.Service;
+//
+//import redis.clients.jedis.Jedis;
+//import redis.clients.jedis.exceptions.JedisException;
+//
+//import java.util.List;
+//import java.util.Set;
+//
+//public class RedisService {
+//    private Jedis jedis;
+//
+//    public RedisService(Jedis jedis) {
+//        this.jedis = jedis;
+//    }
+//
+//    public void increasePageView(Integer customer_Idx) {
+//        try {
+//            jedis.incr(String.valueOf(customer_Idx));
+//        } catch (JedisException e) {
+//            e.printStackTrace();
+//            throw e;
+//        }
+//    }
+//
+//    public Integer getPageView(String customer_Idx) {
+//        try {
+//            String value = jedis.get(customer_Idx);
+//            return value == null ? 0 : Integer.parseInt(value);
+//        } catch (JedisException e) {
+//            e.printStackTrace();
+//            throw e;
+//        }
+//    }
+//
+//    public Set<String> getAllKeys() {
+//        try {
+//            return jedis.keys("*");
+//        } catch (JedisException e) {
+//            e.printStackTrace();
+//            throw e;
+//        }
+//    }
+//
+//
+//    public void addToPreList(Integer customer_Idx) {
+//        jedis.rpush("PreList", String.valueOf(customer_Idx));
+//    }
+//
+//    //lrange jedis연결후 모든 리스트 조회
+//    public List<String> getPreList() {
+//        return jedis.lrange("PreList", 0, -1);
+//    }
+//
+//    public void clearPreList() {
+//        jedis.del("PreList");
+//    }
+//
+//    // 풀이아닌 풀 안에있는 단일 커넥션 제거
+//    public void close() {
+//        if(jedis != null) {
+//            jedis.close();
+//        }
+//    }
+//}
