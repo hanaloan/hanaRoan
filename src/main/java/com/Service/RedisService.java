282aab488241c9e87658c86ac15820d0b4741225
sss_hun <88662101+seonghunhan@users.noreply.github.com>
2023-06-25 14:49:14 +0900

    Feat : Create logic to run individual PK and total PK in the background (#43)

--- src/main/java/com/Service/RedisService.java
+++ src/main/java/com/Service/RedisService.java
@@ -1,27 +1,20 @@
 //package com.Service;
 //
 //import redis.clients.jedis.Jedis;
-//import redis.clients.jedis.JedisPool;
-//import redis.clients.jedis.JedisPoolConfig;
 //import redis.clients.jedis.exceptions.JedisException;
 //
+//import java.util.List;
 //import java.util.Set;
 //
 //public class RedisService {
-//    private JedisPool jedisPool;
+//    private Jedis jedis;
 //
-//    public RedisService(String host) {
-//        JedisPoolConfig poolConfig = new JedisPoolConfig();
-//        poolConfig.setMaxTotal(100); // 최대 커넥션 수
-//        poolConfig.setMaxIdle(10); // 유휴 상태에서 유지하는 최대 커넥션 수
-//        poolConfig.setMinIdle(5); // 유휴 상태에서 유지하는 최소 커넥션 수
-//        poolConfig.setMaxWaitMillis(3000); // 커넥션 풀에서 사용 가능한 커넥션을 기다리는 최대 시간 (밀리초)
-//
-//        this.jedisPool = new JedisPool(poolConfig, host);
+//    public RedisService(Jedis jedis) {
+//        this.jedis = jedis;
 //    }
 //
 //    public void increasePageView(Integer customer_Idx) {
-//        try (Jedis jedis = jedisPool.getResource()) {
+//        try {
 //            jedis.incr(String.valueOf(customer_Idx));
 //        } catch (JedisException e) {
 //            e.printStackTrace();
@@ -29,8 +22,8 @@
 //        }
 //    }
 //
-//    public int getPageView(String customer_Idx) {
-//        try (Jedis jedis = jedisPool.getResource()) {
+//    public Integer getPageView(String customer_Idx) {
+//        try {
 //            String value = jedis.get(customer_Idx);
 //            return value == null ? 0 : Integer.parseInt(value);
 //        } catch (JedisException e) {
@@ -40,11 +33,30 @@
 //    }
 //
 //    public Set<String> getAllKeys() {
-//        try (Jedis jedis = jedisPool.getResource()) {
+//        try {
 //            return jedis.keys("*");
 //        } catch (JedisException e) {
 //            e.printStackTrace();
 //            throw e;
+//        }
+//    }
+//    public void addToPreList(Integer customer_Idx) {
+//        jedis.rpush("PreList", String.valueOf(customer_Idx));
+//    }
+//
+//    //lrange jedis연결후 모든 리스트 조회
+//    public List<String> getPreList() {
+//        return jedis.lrange("PreList", 0, -1);
+//    }
+//
+//    public void clearPreList() {
+//        jedis.del("PreList");
+//    }
+//
+//    // 풀이아닌 풀 안에있는 단일 커넥션 제거
+//    public void close() {
+//        if(jedis != null) {
+//            jedis.close();
 //        }
 //    }
 //}
