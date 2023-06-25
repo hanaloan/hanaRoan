1210c7e8ebfde7a805d83377c8a978c7acb3e386
juseong <jjs7280@gmail.com>
2023-06-25 21:49:49 +0900

    Refactor: 대출 승인 페이지 js코드 분리 및 async,await을 통한 비동기 처리

---
+++ src/main/webapp/js/LoanApproval/LoanApproval.js
@@ -0,0 +1,71 @@
+// 비동기 통신을 위한 공통 함수
+async function sendRequest(url, data) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(data)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+}
+
+async function updateLoanStatus(lendId) {
+    const loanStatus = document.getElementById('loan-status-' + lendId).value;
+
+    if (loanStatus === "approved") {
+        await createLoanPayment(lendId);
+    }
+
+    await sendRequest("/ChangeLoanStatus", { lendId, loanStatus });
+
+    updateDropdownStatus(document.getElementById('loan-status-' + lendId), loanStatus);
+    location.reload();
+}
+
+async function createLoanPayment(lendId) {
+    await sendRequest("/CreateLoanPayment", { lendId });
+    location.reload();
+}
+
+function updateDropdownStatus(selectElement, loanStatus) {
+    selectElement.disabled = loanStatus !== 'pending';
+
+    if (loanStatus === 'denied') {
+        const cells = selectElement.parentNode.parentNode.getElementsByTagName('td');
+        for (let i = 7; i < cells.length; i++) {
+            cells[i].style.backgroundColor = '#F0F0F0';
+            cells[i].style.color = '#999999';
+        }
+    }
+}
+
+window.onload = function () {
+    if (window.location.pathname !== '/LoanApproval') {
+        location.href = '/LoanApproval';
+    }
+
+    const selectElements = document.querySelectorAll("select[id^='loan-status-']");
+    selectElements.forEach((selectElement) => {
+        const lendId = selectElement.id.split('-')[2];
+        const loanStatus = document.getElementById('loan-status-' + lendId).value;
+        updateDropdownStatus(selectElement, loanStatus);
+    });
+}
+
+$(document).ready(function() {
+    const table = $('#dataTable').DataTable();
+
+    function attachDropdownHandlers() {
+        table.$('select[id^="loan-status-"]').off('change').change(function() {
+            const lendId = $(this).attr('id').split('-')[2];
+            const loanStatus = $(this).val();
+            updateLoanStatus(lendId, loanStatus);
+            updateDropdownStatus(this, loanStatus);
+        });
+    }
+
+    table.on('init.dt', attachDropdownHandlers);
+    table.on('draw.dt', attachDropdownHandlers);
+});
